d6d638487ff064ac2359cb34a6c51fc1f06acb3b
Ray Lv <ray.lv@sap.com>
2016-05-11 18:12:53 +0800

    v5

--- housefinance/batch_import_views.py
+++ housefinance/batch_import_views.py
@@ -25,13 +25,14 @@ class ZFBImportView(View):
         return render_to_response(template_name='housefinance/batch_import/batch_import_zfb_step1.html', context=context)
 
     def post(self, request, *args, **kwargs):
-        if request.FILES:
+        import_step = request.POST.get('step')
+        curr_user = User.objects.filter(sys_user=request.user)[:1].get()
+        account_fy = Account.objects.filter(account_type='FY')[:1].get()
+        account_zc = Account.objects.filter(account_type='ZC')[:1].get()
+        if import_step == 'UPLOAD' and request.FILES:
             txtfile = request.FILES['txt_file']
             start_flag = False
             accountingDocuments = []
-            account_fy = Account.objects.filter(account_type='FY')[:1].get()
-            account_zc = Account.objects.filter(account_type='ZC')[:1].get()
-            curr_user = User.objects.filter(sys_user=request.user)[:1].get()
             for line in txtfile:
                 values = line.split(',')
                 if start_flag and len(values) > 14:
@@ -64,14 +65,52 @@ class ZFBImportView(View):
                     if value == '交易号':
                         start_flag = True
                         break
-
             context = {}
             context.update(csrf(request))
             context['acc_docs'] = accountingDocuments
             return render_to_response(template_name='housefinance/batch_import/batch_import_zfb_step2.html', context=context)
-        else:
+        elif import_step == 'IMPORT':
             print(dir(request.POST))
             print(request.POST.get('acc_doc_2_creation_date'))
+            print(request.POST.get('acc_doc_2_checkbox'))
+
+            index_tmp = 1
+
+            while True:
+                if request.POST.get(''.join(['acc_doc_', str(index_tmp)])) is None:
+                    break
+                if request.POST.get(''.join(['acc_doc_', str(index_tmp), '_checkbox'])) is None:
+                    continue
+                acc_doc = AccountingDocumentHeader()
+                acc_doc.creation_date = datetime.strptime(request.POST.get(''.join(['acc_doc_', str(index_tmp), '_creation_date'])), '%Y-%m-%d %H:%M:%S')
+                acc_doc.creator = curr_user
+                acc_doc.comment = request.POST.get(''.join(['acc_doc_', str(index_tmp), '_comment']))
+
+                acc_doc.save()
+
+                acc_item1 = AccountingDocumentItem()
+                acc_item1.dc_indicator = request.POST.get(''.join(['acc_doc_item_', str(index_tmp), '_1_dc_indicator']))
+                acc_item1.amount = request.POST.get(''.join(['acc_doc_item_', str(index_tmp), '_1_amount']))
+                acc_item1.document_header = acc_doc
+                if request.POST.get(''.join(['acc_doc_item_', str(index_tmp), '_1_account_type'])) == 'FY':
+                    acc_item1.account = account_fy
+                elif request.POST.get(''.join(['acc_doc_item_', str(index_tmp), '_1_account_type'])) == 'ZC':
+                    acc_item1.account = account_zc
+
+                acc_item2 = AccountingDocumentItem()
+                acc_item2.dc_indicator = request.POST.get(''.join(['acc_doc_item_', str(index_tmp), '_2_dc_indicator']))
+                acc_item2.amount = request.POST.get(''.join(['acc_doc_item_', str(index_tmp), '_2_amount']))
+                acc_item2.document_header = acc_doc
+                if request.POST.get(''.join(['acc_doc_item_', str(index_tmp), '_2_account_type'])) == 'FY':
+                    acc_item2.account = account_fy
+                elif request.POST.get(''.join(['acc_doc_item_', str(index_tmp), '_2_account_type'])) == 'ZC':
+                    acc_item2.account = account_zc
+
+                acc_item1.save()
+                acc_item2.save()
+
+                index_tmp += 1
+
             return self.get(request, *args, **kwargs)
 
 
